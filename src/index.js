20780371c8f676c216251f0d2a18844c64d1aa05
ritusingh001 <ritu.singh@think.design>
2023-06-08 16:20:54 +0530

    Filtring

--- src/index.js
+++ src/index.js
@@ -2,18 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import CURDData from './Pages/CURD-Data';
+// import CURDData from './Pages/CURD-Data';
 import Filtering from './Pages/Filtering';
 
 const router = createBrowserRouter([
   {
     path:'/',
-    element: <CURDData/>,
-  },
-  {
-    path:'/filtering',
     element: <Filtering/>,
-  },
+  }
 ])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
