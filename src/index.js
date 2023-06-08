671be69089162c12e5ba9805eb2e0fa36c8e38d5
ritusingh001 <ritu.singh@think.design>
2023-06-08 16:17:42 +0530

    Filtering Commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import CURDData from './Pages/CURD-Data';
+import Filtering from './Pages/Filtering';
 
 const router = createBrowserRouter([
   {
     path:'/',
     element: <CURDData/>,
+  },
+  {
+    path:'/filtering',
+    element: <Filtering/>,
   },
 ])
 
